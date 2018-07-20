85eba2b91a2b56cc2b032edda0e7ecffdc577c3d
Sam Sherwood <ssherwood@theadcomgroup.com>
2018-07-20 09:28:53 -0400

    Updated with internal roast cards and their associated imagery

--- js/main.js
+++ js/main.js
@@ -7,25 +7,136 @@ var Flickity = window.Flickity;
 
 
 document.addEventListener('DOMContentLoaded', function () {
-	var cards = [].slice.call(document.querySelectorAll('.cards__card')),
+	var cards = [].slice.call(document.querySelectorAll('.cards--header .cards__card')),
+		modal = document.querySelector('.modal'),
+		modalCards = [].slice.call(document.querySelectorAll('.cards__image--modal')),
+		modalShareBtns = [].slice.call(document.querySelectorAll('.btn--share-card')),
 		modalWrap = document.querySelector('.modal-wrap'),
 		modalClose = document.querySelector('.modal__close-btn');
 
-	cards.forEach(function (card) {
-		card.addEventListener('click', function () {
+
+	app.flkyModal = new Flickity('.cards--modal', {
+		// options, defaults listed
+
+		accessibility: true,
+		// enable keyboard navigation, pressing left & right keys
+
+		adaptiveHeight: false,
+		// set carousel height to the selected slide
+
+		autoPlay: false,
+		// advances to the next cell
+		// if true, default is 3 seconds
+		// or set time between advances in milliseconds
+		// i.e. `autoPlay: 1000` will advance every 1 second
+
+		cellAlign: 'center',
+		// alignment of cells, 'center', 'left', or 'right'
+		// or a decimal 0-1, 0 is beginning (left) of container, 1 is end (right)
+
+		cellSelector: undefined,
+		// specify selector for cell elements
+
+		contain: false,
+		// will contain cells to container
+		// so no excess scroll at beginning or end
+		// has no effect if wrapAround is enabled
+
+		draggable: '>1',
+		// enables dragging & flicking
+		// if at least 2 cells
+
+		dragThreshold: 3,
+		// number of pixels a user must scroll horizontally to start dragging
+		// increase to allow more room for vertical scroll for touch devices
+
+		freeScroll: false,
+		// enables content to be freely scrolled and flicked
+		// without aligning cells
+
+		friction: 0.2,
+		// smaller number = easier to flick farther
+
+		groupCells: false,
+		// group cells together in slides
+
+		initialIndex: 0,
+		// zero-based index of the initial selected cell
+
+		lazyLoad: true,
+		// enable lazy-loading images
+		// set img data-flickity-lazyload="src.jpg"
+		// set to number to load images adjacent cells
+
+		percentPosition: true,
+		// sets positioning in percent values, rather than pixels
+		// Enable if items have percent widths
+		// Disable if items have pixel widths, like images
+
+		prevNextButtons: false,
+		// creates and enables buttons to click to previous & next cells
+
+		pageDots: true,
+		// create and enable page dots
+
+		resize: true,
+		// listens to window resize events to adjust size & positions
+
+		rightToLeft: false,
+		// enables right-to-left layout
+
+		setGallerySize: true,
+		// sets the height of gallery
+		// disable if gallery already has height set with CSS
+
+		watchCSS: false,
+		// watches the content of :after of the element
+		// activates if #element:after { content: 'flickity' }
+
+		wrapAround: true
+		// at end of cells, wraps-around to first for infinite scrolling
+	});
+
+	cards.forEach(function (card, idx) {
+		card.setAttribute('data-card-index', idx);
+		card.addEventListener('click', function (e) {
+			app.flkyModal.select(card.getAttribute('data-card-index'));
 			modalWrap.classList.add('show');
 		});
 	});
 
+	app.flkyModal.on('change', function () {
+		var cardsItems = [].slice.call(document.querySelectorAll('.cards__card--modal'));
+		cardsItems.forEach(function (item) {
+			item.classList.remove('share');
+		});
+
+	});
+
+
+
 	modalClose.addEventListener('click', function () {
 		modalWrap.classList.remove('show');
+
+		modalShareBtns.forEach(function (btn) {
+			btn.parentElement.classList.remove('share');
+		});
+
+		modal.classList.remove('share');
+	});
+
+	modalShareBtns.forEach(function (btn) {
+		btn.addEventListener('click', function (e) {
+			e.preventDefault();
+			btn.parentElement.classList.add('share');
+		}, false);
 	});
 
 	pointsBreak.init({
 		'W<769': function () {
 			if (!app.flkyCards && !app.flkyItinerary) {
 
-				app.flkyCards = new Flickity('.cards', {
+				app.flkyCards = new Flickity('.cards--header', {
 					// options, defaults listed
 
 					accessibility: true,
@@ -201,6 +312,9 @@ document.addEventListener('DOMContentLoaded', function () {
 			}
 		}
 	});
+
+
+
 
 
 
